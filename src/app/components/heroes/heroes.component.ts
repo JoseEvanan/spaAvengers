dde41ee3fc8933415035c91b56830c83e6b07587
Jose Evanan <jose.evanan@gmail.com>
2017-10-31 07:33:57 -0500

    first commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Router } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Heroe[] = [];
+
+  constructor( private _heroesService: HeroesService,
+               private router: Router) {
+    // console.log('constructor');
+  }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    // console.log( this.heroes );
+  }
+
+  verHeroe(idx: number) {
+    console.log(idx);
+    this.router.navigate(['/heroe', idx]);
+  }
+
+}
